0c8341530ba598b26d4c64f41219c5ddd5d1bad3
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2023-06-15 19:50:05 +0900

    fix: delete console code

--- src/middleware.js
+++ src/middleware.js
@@ -4,17 +4,11 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req) {
   let session = await getToken({ req: req });
-  console.log(session);
+  // console.log(session);
 
   if (req.nextUrl.pathname.startsWith("/write")) {
     if (session === null) {
       return NextResponse.redirect("http://localhost:3001/api/auth/signin");
     }
-  }
-
-  if (req.nextUrl.pathname.startsWith("/list")) {
-    console.log(new Date().toString("yyyy-MM-dd HH:mm:ss"));
-    console.log(req.headers);
-    return NextResponse.next();
   }
 }
