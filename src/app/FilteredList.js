f9303c74bd4186dc0f9e83b8c04381ab0a5ddce7
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2023-06-16 19:40:07 +0900

    feat, style: add main page, fix style to module

---
+++ src/app/FilteredList.js
@@ -0,0 +1,72 @@
+"use client";
+
+// filter your content
+
+import Link from "next/link";
+import DetailLink from "@/app/list/DetailLink";
+
+export default function FilteredList({ res, session }) {
+  return (
+    <div>
+      {res.map((data) => {
+        let id = data._id.toString();
+
+        return (
+          <div className="filterListDiv" key={id}>
+            {session.user.name === data.author ? (
+              <div className="filterlistItem" key={id}>
+                <Link href={`/detail/${id}`}>
+                  <h4>{data.title}</h4>
+                </Link>
+                {session ? (
+                  <div className="filterlistBtns">
+                    <DetailLink className="filterdetailBtn" id={id} />
+                    {session.user.name === data.author ? (
+                      <div className="filtereditDelBtn">
+                        <Link className="filtereditBtn" href={`/edit/${id}`}>
+                          Edit
+                        </Link>
+                        <button
+                          className="filterdelBtn"
+                          onClick={(e) => {
+                            fetch("/api/post/delete", {
+                              method: "POST",
+                              body: id,
+                            })
+                              .then((r) => {
+                                r.json();
+                              })
+                              .then(() => {
+                                // console.log(e);
+                                e.target.parentElement.parentElement.parentElement.style.opacity = 0;
+                                setTimeout(() => {
+                                  e.target.parentElement.parentElement.parentElement.style.display =
+                                    "none";
+                                }, 1000);
+                              });
+                          }}
+                        >
+                          Delete
+                        </button>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                ) : (
+                  <div className="filterlistBtns">
+                    <DetailLink className="filterdetailBtn" id={id} />
+                  </div>
+                )}
+
+                <p>{data.author ? data.author : "guest"}</p>
+              </div>
+            ) : (
+              <div style={{ margin: 0, padding: 0, display: "hidden" }}></div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,47 @@
 export const revalidate = 60;
 
+import { clientPromise } from "@/util/database";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import Image from "next/image";
 import defaultImg from "../../public/image/defaultUserImg.png";
+import FilteredList from "./FilteredList";
 
 export default async function Home() {
   let session = await getServerSession(authOptions);
 
+  let client = await clientPromise;
+  let db = client.db("next-board");
+  let res = await db.collection("board").find().toArray();
+
   return (
     <div className="main">
-      {session ? (
-        <div className="mainProfileDiv">
-          <Image
-            width={50}
-            height={50}
-            src={session.user.image ? session.user.image : defaultImg}
-            alt="profile image"
-          />
-          <p>{session.user.name}</p>
-          <p>{session.user.email}</p>
+      <div className="mainProfileDiv">
+        {session ? (
+          <div className="profileContentsDiv">
+            <h3>Profile</h3>
+            <Image
+              width={50}
+              height={50}
+              src={session.user.image ? session.user.image : defaultImg}
+              alt="profile image"
+            />
+            <p className="userName">{session.user.name}</p>
+            <p className="userEmail">{session.user.email}</p>
+          </div>
+        ) : (
+          "Login first"
+        )}
+        <div className="userPageDiv">
+          <button>mypage</button>
         </div>
-      ) : (
-        "this is main page"
-      )}
+      </div>
+      <div className="popularDiv">
+        <div className="popularFilter">
+          <h3>My contents</h3>
+          <FilteredList res={res} session={session} />
+        </div>
+      </div>
     </div>
   );
 }
